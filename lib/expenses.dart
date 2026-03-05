196831afc03bc8ed0c17cf87f5dc5192264dfcd9
DiegoSousaRodrigues <diego.srodrigues2002@gmail.com>
2026-03-05 20:46:04 -0300

    initial commit

---
+++ lib/expenses.dart
@@ -0,0 +1,37 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpenseState();
+}
+
+class _ExpenseState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Flutter',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          Text("test"),
+        ],
+      ),
+    );
+  }
+}
