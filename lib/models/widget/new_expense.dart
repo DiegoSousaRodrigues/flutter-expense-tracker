0a012883cfbd62a59aa2c01f9ad72ee731355b2d
DiegoSousaRodrigues <diego.srodrigues2002@gmail.com>
2026-03-10 19:56:49 -0300

    Enhance NewExpense widget with date picker and amount input functionality

--- lib/models/widget/new_expense.dart
+++ lib/models/widget/new_expense.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
@@ -8,6 +9,27 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  void _presentDatePicker() async {
+    final DateTime now = DateTime.now();
+    final DateTime firstDate = DateTime(now.year - 1);
+
+    final pickerDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,10 +37,50 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            decoration: InputDecoration(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(
               label: Text("Title"),
             ),
-            maxLength: 50,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: '\$ ',
+                    label: Text("Amount"),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  children: [
+                    TextButton.icon(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                      label: const Text("Selected Date"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Spacer(),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text("Cancel"),
+              ),
+              ElevatedButton(
+                onPressed: () => {},
+                child: Text("Save Expense"),
+              ),
+            ],
           ),
         ],
       ),
